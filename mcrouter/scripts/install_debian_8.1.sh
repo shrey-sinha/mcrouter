7f6ed539e3173dc7e9a0acfb33eab1df1fa7a1e9
Shrey Sinha <shrey@zomato.com>
2016-03-31 12:30:45 +0530

    Tweaks for Debain 8

---
+++ mcrouter/scripts/install_debian_8.1.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -ex
+
+[ -n "$1" ] || ( echo "Install dir missing"; exit 1 )
+
+sudo apt-get update
+
+sudo apt-get install -y \
+    autoconf \
+    binutils-dev \
+    bison \
+    flex \
+    g++ \
+    gcc \
+    git \
+    libboost1.55-all-dev \
+    libdouble-conversion-dev \
+    libevent-dev \
+    libgflags-dev \
+    libgoogle-glog-dev \
+    libjemalloc-dev \
+    libssl-dev \
+    libtool \
+    make \
+    python-dev \
+    ragel
+
+cd "$(dirname "$0")" || ( echo "cd fail"; exit 1 )
+
+./get_and_build_everything.sh ubuntu-14.04 "$@"
